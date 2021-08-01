f9f3dec9675332b43d5ca1022fc2d14c3194b54f
edwinjose14 <delgadoedwin1999@gmail.com>
2021-07-31 20:06:29 -0500

    configurando variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME } from './config/constanst'
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'EDWIN',
-      password: '1065850026',
-      database: 'myblog',
-      entities: ["dist/**/**.entity{.ts,.js}"],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService)=> ({
+        type: 'mysql',
+        host: config.get<string>(DB_HOST),
+        port: parseInt(config.get<string>(DB_PORT), 10),
+        username: config.get<string>(DB_USERNAME),
+        password: config.get<string>(DB_PASSWORD),
+        database: config.get<string>(DB_NAME),
+        entities: ["dist/**/**.entity{.ts,.js}"],
+        synchronize: true,
+      })
     }),
     PostModule,
-    UserModule
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
